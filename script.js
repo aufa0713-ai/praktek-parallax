a2236937cd3e8e112607c386f52ed9cad22501c7
aufa0713-ai <aufa0713@student.abudzar.sch.id>
2025-11-14 11:23:31 +0700

    move text

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let Clouds = document.getElementById("Clouds");
 let Mountains = document.getElementById("Mountains");
+let country = document.getElementById("country");
 
 
 window.addEventListener("scroll", () => {
@@ -7,6 +8,8 @@ window.addEventListener("scroll", () => {
 
   Clouds.style.top = value * -0.1 + "px";
   Clouds.style.transform = `scale(${1 + value * 0.001})`;
+  country.style.marginTop = value * 0.5 + "px";
 });
+
 
 
